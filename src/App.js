1c321c0c2ff9185e7efca9b881b82266c6dafce4
JDNaranjo <juannaranjo1206@gmail.com>
2021-12-11 13:22:34 -0500

    Display card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+
+const { Meta } = Card;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300, margin: "auto", position: "absolute", top:"35%", left:"40%"}}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
     </div>
   );
 }
